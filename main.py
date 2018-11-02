6b3b937ca376ea3785741a6ba5a46fd39dc929d8
syado <doi.sinya122@gmail.com>
2018-11-02 10:30:51 +0900

    commit

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message_list = event.message.text.split()
-    reply_mes = talk.talk(event.message.text)
+    reply_mes = talk.talk(event.message.text)["results"][0]["reply"]
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=reply_mes))
